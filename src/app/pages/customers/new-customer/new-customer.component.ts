4f11c0088078f2c472b79321a7a7039c221d3a9d
Eduarda Manenti <eduarda.manenti@nuntec.com.br>
2025-04-24 15:57:58 -0300

    Adicionada integração com via cep.

--- src/app/pages/customers/new-customer/new-customer.component.ts
+++ src/app/pages/customers/new-customer/new-customer.component.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from '../../../components/header/header.component';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 import {  Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-customer',
@@ -11,7 +12,8 @@ import {  Router } from '@angular/router';
   imports: [
     ReactiveFormsModule,
     HeaderComponent,
-    NgxMaskDirective
+    NgxMaskDirective,
+    CommonModule,
   ],
   templateUrl: './new-customer.component.html',
   styleUrl: './new-customer.component.scss',
@@ -35,6 +37,36 @@ export class NewCustomerComponent {
     });
   }
 
+  states = [
+    { abbreviation: 'AC', name: 'Acre' },
+    { abbreviation: 'AL', name: 'Alagoas' },
+    { abbreviation: 'AP', name: 'Amapá' },
+    { abbreviation: 'AM', name: 'Amazonas' },
+    { abbreviation: 'BA', name: 'Bahia' },
+    { abbreviation: 'CE', name: 'Ceará' },
+    { abbreviation: 'DF', name: 'Distrito Federal' },
+    { abbreviation: 'ES', name: 'Espírito Santo' },
+    { abbreviation: 'GO', name: 'Goiás' },
+    { abbreviation: 'MA', name: 'Maranhão' },
+    { abbreviation: 'MT', name: 'Mato Grosso' },
+    { abbreviation: 'MS', name: 'Mato Grosso do Sul' },
+    { abbreviation: 'MG', name: 'Minas Gerais' },
+    { abbreviation: 'PA', name: 'Pará' },
+    { abbreviation: 'PB', name: 'Paraíba' },
+    { abbreviation: 'PR', name: 'Paraná' },
+    { abbreviation: 'PE', name: 'Pernambuco' },
+    { abbreviation: 'PI', name: 'Piauí' },
+    { abbreviation: 'RJ', name: 'Rio de Janeiro' },
+    { abbreviation: 'RN', name: 'Rio Grande do Norte' },
+    { abbreviation: 'RS', name: 'Rio Grande do Sul' },
+    { abbreviation: 'RO', name: 'Rondônia' },
+    { abbreviation: 'RR', name: 'Roraima' },
+    { abbreviation: 'SC', name: 'Santa Catarina' },
+    { abbreviation: 'SP', name: 'São Paulo' },
+    { abbreviation: 'SE', name: 'Sergipe' },
+    { abbreviation: 'TO', name: 'Tocantins' },
+  ];
+
   onSubmit() {
     if (this.customerForm.valid) {
       console.log(this.customerForm.value);
@@ -43,6 +75,37 @@ export class NewCustomerComponent {
 
   goBack() {
     this.router.navigate(["/customers/index"]);
+  }
+
+  searchCep() {
+    const cep = this.customerForm.get('zip')?.value?.replace(/\D/g, '');
+    if (cep?.length === 8) {
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => response.json())
+        .then(data => {
+          if (!data.erro) {
+            this.customerForm.patchValue({
+              street: data.logradouro,
+              city: data.localidade,
+              state: data.uf
+            });
+            this.customerForm.get('street')?.disable();
+            this.customerForm.get('city')?.disable();
+            this.customerForm.get('state')?.disable();
+          } else {
+            this.enableAddressFields();
+          }
+        })
+        .catch(() => {
+          console.error('Erro ao buscar CEP');
+        });
+    }
+  }
+
+  enableAddressFields() {
+    this.customerForm.get('street')?.enable();
+    this.customerForm.get('city')?.enable();
+    this.customerForm.get('state')?.enable();
   }
 
 }
