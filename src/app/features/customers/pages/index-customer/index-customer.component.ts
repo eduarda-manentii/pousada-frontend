d694858dca9dc24165c9f9a1ac48f69860bb5974
Eduarda Manenti <eduarda.manenti@nuntec.com.br>
2025-04-25 09:49:06 -0300

    Migrado telas de clientes para o bootstrap.

--- src/app/features/customers/pages/index-customer/index-customer.component.ts
+++ src/app/features/customers/pages/index-customer/index-customer.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../../../shared/components/header/header.component';
 import { RouterLink } from '@angular/router';
+import { CustomerService } from '../../services/customer.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-index-customer',
   standalone: true,
-  imports: [HeaderComponent, RouterLink],
+  imports: [HeaderComponent, RouterLink, CommonModule],
   templateUrl: './index-customer.component.html',
   styleUrl: './index-customer.component.scss'
 })
-export class IndexCustomerComponent {
+export class IndexCustomerComponent implements OnInit {
+  customers: any[] = [];
 
+  constructor(private customerService: CustomerService) {}
+
+  ngOnInit() {
+    this.customerService.getCustomers().subscribe((data) => {
+      this.customers = data;
+    });
+  }
 }
--- src/app/features/customers/services/customer.service.ts
+++ src/app/features/customers/services/customer.service.ts
@@ -1,16 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
   private apiUrl = 'http://localhost:8080/cliente';
+  private STORAGE_KEY = 'customers';
+  private useLocal = true;
 
   constructor(private http: HttpClient) {}
 
   createCustomer(customer: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, customer);
+    if (this.useLocal) {
+      const customers = this.getCustomersSync();
+      customers.push(customer);
+      localStorage.setItem(this.STORAGE_KEY, JSON.stringify(customers));
+      return of(customer);
+    } else {
+      return this.http.post<any>(this.apiUrl, customer);
+    }
+  }
+
+  getCustomers(): Observable<any[]> {
+    if (this.useLocal) {
+      const customers = this.getCustomersSync();
+      return of(customers);
+    } else {
+      return this.http.get<any[]>(this.apiUrl);
+    }
+  }
+
+  private getCustomersSync(): any[] {
+    const data = localStorage.getItem(this.STORAGE_KEY);
+    return data ? JSON.parse(data) : [];
   }
 }
