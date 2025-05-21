54d5c4f4e8c5cc5f7d1e2af06ada345f758dbdbc
EDUARDA NUNES MANENTI <eduarda_manenti@estudante.sc.senai.br>
2025-05-21 20:19:29 -0300

    Criado tela de cadastro de amenidade.

--- src/app/features/amenities/pages/index-amenity/index-amenity.component.ts
+++ src/app/features/amenities/pages/index-amenity/index-amenity.component.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../../../shared/components/header/header.component';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CapitalizePipe } from '../../../../shared/pipes/capitalize.pipe';
+import { FilterModalComponent } from '../../../../shared/components/filter-modal/filter-modal.component';
+import { FiltroConfig } from '../../../../shared/interfaces/filtro-config';
+import { useList } from '../../../../shared/composables/use-list';
 
 @Component({
   selector: 'app-index-amenity',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [
+    HeaderComponent,
+    RouterLink,
+    CommonModule,
+    CapitalizePipe,
+    FilterModalComponent
+  ],
   templateUrl: './index-amenity.component.html',
   styleUrl: './index-amenity.component.scss'
 })
 export class IndexAmenityComponent {
+  filtroAmenidades: FiltroConfig[] = [
+      { key: 'nome', label: 'Nome da Amenidade', type: 'text' }
+  ];
+
+  private list = useList<any>('/amenidades', (a, b) => a.nome.localeCompare(b.nome));
+
+  amenities = this.list.items;
+  currentPage = this.list.currentPage;
+  totalPages = this.list.totalPages;
+
+  ngOnInit() {
+    this.list.loadPage(0);
+  }
+
+  aplicarFiltros(filtros: any) {
+    this.list.applyFilters(filtros);
+  }
+
+  nextPage() {
+    this.list.nextPage();
+  }
+
+  previousPage() {
+    this.list.previousPage();
+  }
 
 }
--- src/app/features/amenities/pages/new-amenity/new-amenity.component.ts
+++ src/app/features/amenities/pages/new-amenity/new-amenity.component.ts
@@ -1,13 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from '../../../../shared/components/header/header.component';
+import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
+import { ApiService } from '../../../../shared/services/backend-api.service';
+import { RequiredMarkerDirective } from '../../../../shared/directives/required-marker.directive';
+import { iconOptions } from '../../interfaces/icon-options';
 
 @Component({
   selector: 'app-new-amenity',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [
+    ReactiveFormsModule,
+    HeaderComponent,
+    CommonModule,
+    RequiredMarkerDirective
+  ],
   templateUrl: './new-amenity.component.html',
   styleUrl: './new-amenity.component.scss'
 })
-export class NewAmenityComponent {
+export class NewAmenityComponent implements OnInit {
+  amenityForm!: FormGroup;
+  amenityId?: number;
+  iconOptions = iconOptions;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder,
+    private api: ApiService,
+    private toastService: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.amenityId = Number(this.route.snapshot.paramMap.get('id'));
+    this.buildForm();
+
+    if (this.amenityId) {
+      this.api.getById('/amenities/' + this.amenityId).then((amenity: any) => {
+        this.amenityForm.patchValue(amenity);
+      });
+    }
+  }
+
+  buildForm() {
+    this.amenityForm = this.fb.group({
+      nome: ['', Validators.required],
+      icon: ['', Validators.required]
+    });
+  }
+
+  async onSubmit() {
+    if (this.amenityForm.valid) {
+      try {
+        const data = this.amenityForm.value;
+
+        if (this.amenityId) {
+          await this.api.put(`/amenities/${this.amenityId}`, data);
+          this.toastService.success('Amenidade atualizada com sucesso!');
+        } else {
+          await this.api.create('/amenities', data);
+          this.toastService.success('Amenidade criada com sucesso!');
+        }
+
+        this.router.navigate(['/amenities/index']);
+      } catch (err: any) {
+        this.toastService.error('Erro ao salvar a amenidade.');
+      }
+    } else {
+      this.toastService.error('Preencha todos os campos obrigatórios.');
+    }
+  }
+
+  goBack() {
+    this.router.navigate(['/amenities/index']);
+  }
 
 }
