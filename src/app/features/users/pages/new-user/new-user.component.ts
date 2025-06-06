7cd5e3d15725a300ce46b77f21e257e3a5e914b1
MuriloVieiraCruz <murilo12super@gmail.com>
2025-06-05 21:21:57 -0300

    Retirado campo de role na tela de usuários

--- src/app/features/users/pages/new-user/new-user.component.ts
+++ src/app/features/users/pages/new-user/new-user.component.ts
@@ -1,29 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../../../shared/components/header/header.component';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { NgxMaskDirective } from 'ngx-mask';
 import { CommonModule } from '@angular/common';
 import { RequiredMarkerDirective } from '../../../../shared/directives/required-marker.directive';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from '../../../../shared/services/backend-api.service';
 import { ToastrService } from 'ngx-toastr';
 import { User } from '../../interfaces/Users';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { map, Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-new-user',
   standalone: true,
   imports: [
-    MatAutocompleteModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatButtonModule,
     ReactiveFormsModule,
     HeaderComponent, 
     ReactiveFormsModule,
@@ -37,8 +25,6 @@ export class NewUserComponent implements OnInit {
 
   userForm!: FormGroup;
   userId?: number | undefined;
-  roles: string[] = ['Administrador', 'Funcionário', 'Gerente'];
-  filteredRoles: string[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -66,28 +52,7 @@ export class NewUserComponent implements OnInit {
       nome: ['', [Validators.required, Validators.minLength(1)]],
       email: ['', [Validators.required, Validators.email]],
       senha: ['', [Validators.required]],
-      roles: [, [Validators.required]]
     });
-  }
-
-  onRoleInput() {
-    const value = this.userForm.get('roles')?.value?.toLowerCase() || '';
-
-    if (value) {
-      this.filteredRoles = this.roles.filter(role => 
-        role.toLowerCase().includes(value)
-      );
-    }
-  }
-
-  selectRole(role: string) {
-    this.userForm.get('roles')?.setValue(role);
-    this.filteredRoles = [];
-  }
-
-  openRoleModal() {
-    console.log('Abrir modal de criação de role...');
-    // Aqui você chamará a modal futuramente
   }
 
   async onSubmit() {
