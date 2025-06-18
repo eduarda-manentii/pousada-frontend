fca3ae95f374a76ed4ea6d585957bac172e880ef
Eduarda Manenti <dudamanenti65@gmail.com>
2025-06-18 19:03:54 -0300

    Corrigido local storage.

--- src/app/core/services/auth-guard.service.ts
+++ src/app/core/services/auth-guard.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +8,15 @@ export class AuthGuard implements CanActivate {
 
   constructor(private router: Router) {}
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    const authToken = sessionStorage.getItem('auth-token');
-
-    if (authToken) {
+  canActivate(): boolean {
+    const token = localStorage.getItem('auth_token'); 
+    if (token) {
       return true;
-    } else {
-      this.router.navigate(['/login']);
-      return false;
     }
+
+    this.router.navigate(['/login']);
+    return false;
   }
+
   
 }
