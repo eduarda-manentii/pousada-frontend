acc94e93cf481274f3c9e5312612b7cdc5b718ac
eduarda-manentii <dudamanenti65@gmail.com>
2025-08-19 21:33:40 -0300

    Adicionado filtros na tela de relatório de reserva por período.

--- src/app/features/reports/pages/reservations-report/reservations-report.component.ts
+++ src/app/features/reports/pages/reservations-report/reservations-report.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../../../shared/components/header/header.component';
 import { useList } from '../../../../shared/composables/use-list';
 import { FiltroConfig } from '../../../../shared/interfaces/filtro-config';
 import { FilterModalComponent } from '../../../../shared/components/filter-modal/filter-modal.component';
+import { ApiService } from '../../../../shared/services/backend-api.service';
+import { Cliente } from '../../../customers/interfaces/cliente';
+import { Quarto } from '../../../rooms/interfaces/quarto';
 
 @Component({
   selector: 'app-reservations-report',
@@ -14,13 +17,58 @@ import { FilterModalComponent } from '../../../../shared/components/filter-modal
   templateUrl: './reservations-report.component.html',
   styleUrl: './reservations-report.component.scss'
 })
-export class ReservationsReportComponent {
+export class ReservationsReportComponent implements OnInit {
+
+  constructor(
+    private api: ApiService
+  ) {}
+
   filtroRelatorio: FiltroConfig[] = [
-    { key: 'nome', label: 'Nome do Quarto', type: 'text' },
-    { key: 'status', label: 'Status', type: 'select' },
-    { keys: ['checkIn', 'checkOut'], label: 'Período', type: 'range', subtype: 'date'},
+    { key: 'quarto', label: 'Quarto', type: 'select', options: [] },
+    { key: 'cliente', label: 'Cliente', type: 'select', options: [] },
+    { key: 'statusDaReserva', label: 'Status', type: 'select', options: [] },
+    { keys: ['checkIn', 'checkOut'], label: 'Período', type: 'range', subtype: 'date'}
   ];
+
   private list = useList<any>('/reservas', (a, b) => new Date(a.checkIn).getTime() - new Date(b.checkIn).getTime());
+
+  ngOnInit(): void {
+    this.loadSelectOptions();
+  }
+
+  async loadSelectOptions() {
+    try {
+      const quartosPage = await this.api.get<Quarto>('/quartos');
+      const clientesPage = await this.api.get<Cliente>('/clientes');
+
+      const quartoOptions = quartosPage.content.map(q => ({
+        label: q.nome,
+        value: q.id
+      }));
+
+      const clienteOptions = clientesPage.content.map(c => ({
+        label: c.nome,
+        value: c.id
+      }));
+      
+      const statusOptions = [
+        { label: 'Aberta', value: 'ABERTA' },
+        { label: 'Fechada', value: 'FECHADA' },
+        { label: 'Cancelada', value: 'CANCELADA' },
+        { label: 'Concluida', value: 'CONCLUIDA' }
+      ];
+
+      this.filtroRelatorio = this.filtroRelatorio.map(f => {
+        if (f.key === 'quarto') return { ...f, options: quartoOptions };
+        if (f.key === 'cliente') return { ...f, options: clienteOptions };
+        if (f.key === 'statusDaReserva') return { ...f, options: statusOptions };
+        return f;
+      });
+
+    } catch (err) {
+      console.error('Erro ao carregar opções de filtros', err);
+    }
+  }
 
   aplicarFiltros(filtros: any) {
     this.list.applyFilters(filtros);
