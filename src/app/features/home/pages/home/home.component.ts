a4ee1625cc43a22fb1e2b83823786778a83cfd3b
eduarda-manentii <dudamanenti65@gmail.com>
2025-08-27 11:34:31 -0300

    Adicionado tabela de locação do dia na tela principal.

--- src/app/features/home/pages/home/home.component.ts
+++ src/app/features/home/pages/home/home.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../../../../shared/components/header/header.component';
+import { useList } from '../../../../shared/composables/use-list';
+import { ApiService } from '../../../../shared/services/backend-api.service';
+import { Cliente } from '../../../customers/interfaces/cliente';
+import { Quarto } from '../../../rooms/interfaces/Quarto';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, HeaderComponent],
+  imports: [RouterModule, HeaderComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  locacoesDoDia: any[] = [];
+  private list = useList<any>('/reservas', (a, b) => new Date(a.checkIn).getTime() - new Date(b.checkIn).getTime());
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit() {
+    this.list.loadPage(0).then(() => {
+      this.filtrarLocacoesDoDia();
+    });
+  }
+
+  private filtrarLocacoesDoDia() {
+    const hoje = new Date();
+    hoje.setHours(0,0,0,0);
+
+    this.locacoesDoDia = this.list.items().filter(reserva => {
+      const checkIn = new Date(reserva.checkIn);
+      checkIn.setHours(0,0,0,0);
+      return checkIn.getTime() === hoje.getTime();
+    });
+  }
 
 }
